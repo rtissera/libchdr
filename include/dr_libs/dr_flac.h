ef64b3d21bd86c053b52d43275456c06ccd0bad0
scribam <scribam@users.noreply.github.com>
2022-02-07 18:39:44 +0100

    dr_flac: Version 0.12.36
    Fix a compilation error with the ARM build.

--- include/dr_libs/dr_flac.h
+++ include/dr_libs/dr_flac.h
@@ -1,6 +1,6 @@
 /*
 FLAC audio decoder. Choice of public domain or MIT-0. See license statements at the end of this file.
-dr_flac - v0.12.35 - 2022-02-06
+dr_flac - v0.12.36 - 2022-02-07
 
 David Reid - mackron@gmail.com
 
@@ -232,7 +232,7 @@ extern "C" {
 
 #define DRFLAC_VERSION_MAJOR     0
 #define DRFLAC_VERSION_MINOR     12
-#define DRFLAC_VERSION_REVISION  35
+#define DRFLAC_VERSION_REVISION  36
 #define DRFLAC_VERSION_STRING    DRFLAC_XSTRINGIFY(DRFLAC_VERSION_MAJOR) "." DRFLAC_XSTRINGIFY(DRFLAC_VERSION_MINOR) "." DRFLAC_XSTRINGIFY(DRFLAC_VERSION_REVISION)
 
 #include <stddef.h> /* For size_t. */
@@ -4782,7 +4782,7 @@ static drflac_bool32 drflac__decode_samples_with_residual__rice__neon(drflac_bs*
     DRFLAC_ASSERT(pSamplesOut != NULL);
 
     /* In my testing the order is rarely > 12, so in this case I'm going to simplify the NEON implementation by only handling order <= 12. */
-    if (order > 0 && order <= 12) {
+    if (lpcOrder > 0 && lpcOrder <= 12) {
         if (drflac__use_64_bit_prediction(bitsPerSample, lpcOrder, lpcPrecision)) {
             return drflac__decode_samples_with_residual__rice__neon_64(bs, count, riceParam, lpcOrder, lpcShift, coefficients, pSamplesOut);
         } else {
@@ -11938,6 +11938,9 @@ DRFLAC_API drflac_bool32 drflac_next_cuesheet_track(drflac_cuesheet_track_iterat
 /*
 REVISION HISTORY
 ================
+v0.12.36 - 2022-02-07
+  - Fix a compilation error with the ARM build.
+
 v0.12.35 - 2022-02-06
   - Fix a bug due to underestimating the amount of precision required for the prediction stage.
   - Fix some bugs found from fuzz testing.

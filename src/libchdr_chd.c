dc6344b2cfadabf2f0218a82b299d57b067a222c
Romain TISSERAND <romain.tisserand@gmail.com>
2025-06-27 00:22:33 +0200

    Bump zstd to 1.5.7, replace by single-file decoder zstddeclib.c

--- src/libchdr_chd.c
+++ src/libchdr_chd.c
@@ -48,7 +48,12 @@
 #include <libchdr/cdrom.h>
 #include <libchdr/flac.h>
 #include <libchdr/huffman.h>
+
+#ifdef USE_SYSTEM_ZSTD
 #include <zstd.h>
+#else
+#include <zstddeclib.c>
+#endif
 
 #include "LzmaEnc.h"
 #include "LzmaDec.h"

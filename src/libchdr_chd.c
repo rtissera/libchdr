cb87905f7188f5923a6331f2b10c612d7d0b9363
Clownacy <Clownacy@users.noreply.github.com>
2026-02-05 21:48:53 +0000

    Split CDFL codec to its own file.

--- src/libchdr_chd.c
+++ src/libchdr_chd.c
@@ -46,6 +46,7 @@
 
 #include "../include/libchdr/chd.h"
 #include "../include/libchdr/cdrom.h"
+#include "../include/libchdr/codec_cdfl.h"
 #include "../include/libchdr/codec_cdlz.h"
 #include "../include/libchdr/codec_cdzl.h"
 #include "../include/libchdr/codec_cdzs.h"
@@ -193,18 +194,6 @@ struct _huff_codec_data
 	struct huffman_decoder* decoder;
 };
 
-/* codec-private data for the CDFL codec */
-typedef struct _cdfl_codec_data cdfl_codec_data;
-struct _cdfl_codec_data {
-	/* internal state */
-	int		swap_endian;
-	flac_decoder	decoder;
-#ifdef WANT_SUBCODE
-	zlib_codec_data		subcode_decompressor;
-#endif
-	uint8_t*	buffer;
-};
-
 /* internal representation of an open CHD file */
 struct _chd_file
 {
@@ -277,11 +266,6 @@ static chd_error huff_codec_init(void *codec, uint32_t hunkbytes);
 static void huff_codec_free(void *codec);
 static chd_error huff_codec_decompress(void *codec, const uint8_t *src, uint32_t complen, uint8_t *dest, uint32_t destlen);
 
-/* cdfl compression codec */
-static chd_error cdfl_codec_init(void* codec, uint32_t hunkbytes);
-static void cdfl_codec_free(void* codec);
-static chd_error cdfl_codec_decompress(void *codec, const uint8_t *src, uint32_t complen, uint8_t *dest, uint32_t destlen);
-
 /***************************************************************************
  *  HUFFMAN DECOMPRESSOR
  ***************************************************************************
@@ -324,99 +308,6 @@ static chd_error huff_codec_decompress(void *codec, const uint8_t *src, uint32_t
 
 	free(bitbuf);
 	return result;
-}
-
-static uint32_t cdfl_codec_blocksize(uint32_t bytes)
-{
-	/* for CDs it seems that CD_MAX_SECTOR_DATA is the right target */
-	uint32_t blocksize = bytes / 4;
-	while (blocksize > CD_MAX_SECTOR_DATA)
-		blocksize /= 2;
-	return blocksize;
-}
-
-static chd_error cdfl_codec_init(void *codec, uint32_t hunkbytes)
-{
-#ifdef WANT_SUBCODE
-	chd_error ret;
-#endif
-	cdfl_codec_data *cdfl = (cdfl_codec_data*)codec;
-
-	/* make sure the CHD's hunk size is an even multiple of the frame size */
-	if (hunkbytes % CD_FRAME_SIZE != 0)
-		return CHDERR_CODEC_ERROR;
-
-	cdfl->buffer = (uint8_t*)malloc(sizeof(uint8_t) * hunkbytes);
-	if (cdfl->buffer == NULL)
-		return CHDERR_OUT_OF_MEMORY;
-
-	/* determine whether we want native or swapped samples */
-	cdfl->swap_endian = flac_decoder_detect_native_endian();
-
-#ifdef WANT_SUBCODE
-	/* init zlib inflater */
-	ret = zlib_codec_init(&cdfl->subcode_decompressor, (hunkbytes / CD_FRAME_SIZE) * CD_MAX_SECTOR_DATA);
-	if (ret != CHDERR_NONE)
-		return ret;
-#endif
-
-	/* flac decoder init */
-	if (flac_decoder_init(&cdfl->decoder))
-		return CHDERR_OUT_OF_MEMORY;
-
-	return CHDERR_NONE;
-}
-
-static void cdfl_codec_free(void *codec)
-{
-	cdfl_codec_data *cdfl = (cdfl_codec_data*)codec;
-	flac_decoder_free(&cdfl->decoder);
-#ifdef WANT_SUBCODE
-	zlib_codec_free(&cdfl->subcode_decompressor);
-#endif
-	if (cdfl->buffer)
-		free(cdfl->buffer);
-}
-
-static chd_error cdfl_codec_decompress(void *codec, const uint8_t *src, uint32_t complen, uint8_t *dest, uint32_t destlen)
-{
-	uint32_t framenum;
-	uint8_t *buffer;
-#ifdef WANT_SUBCODE
-	uint32_t offset;
-	chd_error ret;
-#endif
-	cdfl_codec_data *cdfl = (cdfl_codec_data*)codec;
-
-	/* reset and decode */
-	uint32_t frames = destlen / CD_FRAME_SIZE;
-
-	if (!flac_decoder_reset(&cdfl->decoder, 44100, 2, cdfl_codec_blocksize(frames * CD_MAX_SECTOR_DATA), src, complen))
-		return CHDERR_DECOMPRESSION_ERROR;
-	buffer = &cdfl->buffer[0];
-	if (!flac_decoder_decode_interleaved(&cdfl->decoder, (int16_t *)(buffer), frames * CD_MAX_SECTOR_DATA/4, cdfl->swap_endian))
-		return CHDERR_DECOMPRESSION_ERROR;
-
-#ifdef WANT_SUBCODE
-	/* inflate the subcode data */
-	offset = flac_decoder_finish(&cdfl->decoder);
-	ret = zlib_codec_decompress(&cdfl->subcode_decompressor, src + offset, complen - offset, &cdfl->buffer[frames * CD_MAX_SECTOR_DATA], frames * CD_MAX_SUBCODE_DATA);
-	if (ret != CHDERR_NONE)
-		return ret;
-#else
-	flac_decoder_finish(&cdfl->decoder);
-#endif
-
-	/* reassemble the data */
-	for (framenum = 0; framenum < frames; framenum++)
-	{
-		memcpy(&dest[framenum * CD_FRAME_SIZE], &cdfl->buffer[framenum * CD_MAX_SECTOR_DATA], CD_MAX_SECTOR_DATA);
-#ifdef WANT_SUBCODE
-		memcpy(&dest[framenum * CD_FRAME_SIZE + CD_MAX_SECTOR_DATA], &cdfl->buffer[frames * CD_MAX_SECTOR_DATA + framenum * CD_MAX_SUBCODE_DATA], CD_MAX_SUBCODE_DATA);
-#endif
-	}
-
-	return CHDERR_NONE;
 }
 
 

8ef2dfc5fe4a0928561a7af741a48d915c988438
Clownacy <Clownacy@users.noreply.github.com>
2026-03-07 23:16:55 +0000

    Allow dependent projects to enable miniz functionality.
    ClownMDEmu uses the bundled copy of miniz to load ZIP files, but
    needs its archive API to do so. Without this, a nasty CMake hack is
    needed to filter the `miniz` target's `COMPILE_DEFINITIONS` property.

--- unity.c
+++ unity.c
@@ -1,8 +1,20 @@
-/* Disable unused features of miniz. */
+/* Disable unused features of miniz (but allow
+   them to be restored by dependent projects). */
+#ifndef MINIZ_ARCHIVE_APIS
 #define MINIZ_NO_ARCHIVE_APIS
+#endif
+
+#ifndef MINIZ_DEFLATE_APIS
 #define MINIZ_NO_DEFLATE_APIS
+#endif
+
+#ifndef MINIZ_STDIO
 #define MINIZ_NO_STDIO
+#endif
+
+#ifnfef MINIZ_TIME
 #define MINIZ_NO_TIME
+#endif
 
 #include "deps/lzma-25.01/src/LzmaDec.c"
 #include "deps/miniz-3.1.1/miniz.c"

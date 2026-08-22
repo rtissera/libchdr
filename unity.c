f6fdbbd20f8b041ec09d1fda01e3e3f55b0f0437
scribam <scribam@users.noreply.github.com>
2026-08-22 09:42:03 +0200

    Update miniz from 3.1.1 to 3.1.2

--- unity.c
+++ unity.c
@@ -17,7 +17,7 @@
 #endif
 
 #include "deps/lzma-25.01/src/LzmaDec.c"
-#include "deps/miniz-3.1.1/miniz.c"
+#include "deps/miniz-3.1.2/miniz.c"
 #include "deps/zstd-1.5.7/zstddeclib.c"
 
 #include "src/libchdr_bitstream.c"
